f2be7cc446f9938d7b86ce8645d86a015643d876
Damoon Saghian <damoonsaghian84@gmail.com>
2026-05-01 00:22:52 +0330

    Delete uni/Ubuild.sh

--- uni/Ubuild.sh
+++
@@ -1,9 +0,0 @@
-upm_import qt
-upm_import quickshell # needed for Process qml type
-upm_import curl
-upm_import mauikit-filebrowsing
-upm_import archive
-
-upm_xcript inst/app uni
-
-ln "$pkg_dir/.data/uni.svg" "$pkg_dir/.cache/upm/$ARCH/app/uni.svg"
