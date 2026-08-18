22092b96f49af5efdcf91a4ce8e1af1ec18a1721
Damoon Saghian <damoonsaghian84@gmail.com>
2026-08-18 23:54:46 +0330

    Add files via upload

--- chimera.sh
+++ chimera.sh
@@ -102,6 +102,26 @@ echo 'permit nu cmd /usr/bin/passwd nu' > "$new_root"/etc/doas.d/passwd.conf
 echo 'GETTY_ARGS="$GETTY_ARGS --autologin nu"' > "$new_root"/etc/default/agetty-tty1
 cp "$new_root"/etc/default/agetty-tty1 "$new_root"/etc/default/agetty-tty2
 
+echo '#!/usr/bin/env sh
+# bash secured by not running bashrc and profile in home directory
+if [ "$1" = "-l" ]; then
+	/usr/bin/bash --rcfile /etc/profile
+else
+	/usr/bin/bash --rcfile /usr/share/bash/bashrc
+fi
+' > "$new_root"/usr/local/bin/bash-sec
+chmod +x "$new_root"/usr/local/bin/bash-sec
+
+cat <<-'EOF' > "$new_root"/etc/bash/bashrc.d/prompt.sh
+PS1='\[$(
+IFS="[;" read -p $"\e[6n" -d R -rs _ _ line _
+[ "$line" = 1 ] || echo
+printf "%0.s─" $(seq 1 $((COLUMNS/2 - ${#PWD}/2 - 1)) )
+)\]\e[7m \[${PWD}\] \e[0m\[$(printf "%0.s─" $(seq 1 $((COLUMNS - COLUMNS/2 + ${#PWD}/2 + 2 - ${#PWD} - 3)) ))\]\n'
+PS2=""
+PS0='\[$(printf "%0.s-" $(seq 1 $((COLUMNS)) ))\]\n'
+EOF
+
 script_dir="$(dirname "$(readlink -f "$0")")"
 
 cp -r "$script_dir"/ushell "$new_root"/usr/local/share/
